ab2c43ce4f5a39c81364163f5f8d32aaafbd179d
John Arkema <john.arkema@gmail.com>
2021-09-17 15:01:53 -0700

    Redirect after error

--- src/ErrorBoundary.js
+++ src/ErrorBoundary.js
@@ -1,31 +1,37 @@
 // Error boundaries ONLY work in class components!!
-import { Component } from 'react';
-import { Link } from 'react-router-dom';
+import { Component } from 'react'
+import { Link } from 'react-router-dom'
+import { Redirect } from 'react-router-dom/cjs/react-router-dom.min'
 
 class ErrorBoundary extends Component {
-	state = { hasError: false };
+	state = { hasError: false, redirect: false }
 
 	// can pass e into function
 	static getDerivedStateFromError() {
-		return { hasError: true };
+		return { hasError: true }
 	}
 
 	componentDidCatch(error, info) {
 		// log to Sentry/TrackJS - somekind of error monitor
-		console.error('ErrorBoundary caught error', error, info);
-	}
-	render() {
+		console.error('ErrorBoundary caught error', error, info)
+
 		if (this.state.hasError) {
-			return (
-				<h2>
-					This Listing encountered an error.
-					<Link to='/'>Click Here to go home</Link>
-				</h2>
-			);
+			setTimeout(() => {
+				this.setState({ redirect: true })
+			}, 5000)
+		}
+	}
+
+	render() {
+		if (this.state.redirect) {
+			return <Redirect to='/' />
+		}
+		if (this.state.hasError) {
+			return <h2>This Listing encountered an error. Please Wait</h2>
 		}
 		//
-		return this.props.children;
+		return this.props.children
 	}
 }
 
-export default ErrorBoundary;
+export default ErrorBoundary
