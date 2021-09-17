f685daa24d3fa34e791ecc54dcadf06ea517a1fa
John Arkema <john.arkema@gmail.com>
2021-09-17 14:26:33 -0700

    Create interactive carousel

--- src/Details.js
+++ src/Details.js
@@ -1,5 +1,6 @@
 import { Component } from 'react';
 import { withRouter } from 'react-router-dom';
+import Carousel from './Carousel';
 
 class Details extends Component {
 	// default state of component
@@ -12,25 +13,26 @@ class Details extends Component {
 			`http://pets-v2.dev-apis.com/pets?id=${this.props.match.params.id}`
 		);
 		const json = await res.json();
-		// this.setState(
-		// 	Object.assign(
-		// 		{
-		// 			loading: false,
-		// 		},
-		// 		json.pets[0]
-		// 	)
-		// );
+		this.setState(
+			Object.assign(
+				{
+					loading: false,
+				},
+				json.pets[0]
+			)
+		);
 
 		// verbose, know exactly what to expect from API
-		this.setState({
-			loading: false,
-			name: json.pets[0].name,
-			breed: json.pets[0].breed,
-			animal: json.pets[0].animal,
-			city: json.pets[0].city,
-			state: json.pets[0].state,
-			description: json.pets[0].description,
-		});
+		// this.setState({
+		// 	loading: false,
+		// 	name: json.pets[0].name,
+		// 	breed: json.pets[0].breed,
+		// 	animal: json.pets[0].animal,
+		// 	city: json.pets[0].city,
+		// 	state: json.pets[0].state,
+		// 	description: json.pets[0].description,
+		// 	images: json.pets[0].images,
+		// });
 	}
 
 	render() {
@@ -38,10 +40,12 @@ class Details extends Component {
 			return <h2>Loading</h2>;
 		}
 		// deconstruct
-		const { animal, breed, city, state, description, name } = this.state;
+		const { animal, breed, city, state, description, name, images } =
+			this.state;
 
 		return (
 			<div className='details'>
+				<Carousel images={images} />
 				<div>
 					<h1>{name}</h1>
 					<h2>
