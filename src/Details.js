2885850092b2b3ff57408918a7240e227fcecee8
John Arkema <john.arkema@gmail.com>
2021-09-22 14:54:13 -0700

    Implement Modal/Portals

--- src/Details.js
+++ src/Details.js
@@ -3,11 +3,12 @@ import { withRouter } from 'react-router-dom';
 import Carousel from './Carousel';
 import ErrorBoundary from './ErrorBoundary';
 import ThemeContext from './ThemeContext';
+import Modal from './Modal';
 
 class Details extends Component {
 	// default state of component
 	// use babel parser to fix error
-	state = { loading: true };
+	state = { loading: true, showModal: false };
 
 	async componentDidMount() {
 		//data fetch
@@ -37,12 +38,16 @@ class Details extends Component {
 		// });
 	}
 
+	toggleModal = () => this.setState({ showModal: !this.state.showModal });
+
+	adopt = () => (window.location = 'http://bit.ly/pet-adopt');
+
 	render() {
 		if (this.state.loading) {
 			return <h2>Loading</h2>;
 		}
 		// deconstruct
-		const { animal, breed, city, state, description, name, images } =
+		const { animal, breed, city, state, description, name, images, showModal } =
 			this.state;
 
 		return (
@@ -55,13 +60,41 @@ class Details extends Component {
 					</h2>
 					<ThemeContext.Consumer>
 						{([themeHook]) => (
-							<button style={{ backgroundColor: themeHook }}>
+							<button
+								style={{ backgroundColor: themeHook }}
+								onClick={this.toggleModal}
+							>
 								Adopt {name}
 							</button>
 						)}
 					</ThemeContext.Consumer>
 
 					<p>{description}</p>
+					{showModal ? (
+						<Modal>
+							<div>
+								<h1>Would you like to adopt {name}?</h1>
+								<ThemeContext.Consumer>
+									{([themeHook]) => (
+										<div className='buttons'>
+											<button
+												style={{ backgroundColor: themeHook }}
+												onClick={this.adopt}
+											>
+												Yes
+											</button>
+											<button
+												style={{ backgroundColor: themeHook }}
+												onClick={this.toggleModal}
+											>
+												No thanks.{' '}
+											</button>
+										</div>
+									)}
+								</ThemeContext.Consumer>
+							</div>
+						</Modal>
+					) : null}
 				</div>
 			</div>
 		);
